cc8848e4425dc36a16a0058b6773b1efc3492944
Davide DS <davidedisilvestro99@gmail.com>
2022-11-09 09:48:33 +0100

    Create Distribution HW6

--- HW6/DistributionMeanVariance/Form1.Designer.vb
+++ HW6/DistributionMeanVariance/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,67 +20,50 @@ Partial Class Form1
     'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
     'Può essere modificata in Progettazione Windows Form.  
     'Non modificarla mediante l'editor del codice.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ButtonMean = New System.Windows.Forms.Button()
-        Me.ButtonVariance = New System.Windows.Forms.Button()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.ButtonMeanVariance = New System.Windows.Forms.Button()
         Me.ButtonClose = New System.Windows.Forms.Button()
-        Me.RichTextBoxMean = New System.Windows.Forms.RichTextBox()
-        Me.RichTextBoxVariance = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBoxM_V = New System.Windows.Forms.RichTextBox()
         Me.TextBoxN = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.PictureBoxMean = New System.Windows.Forms.PictureBox()
-        Me.PictureBoxVariance = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBoxMean, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBoxVariance, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ChartMean = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        CType(Me.ChartMean, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'ButtonMean
+        'ButtonMeanVariance
         '
-        Me.ButtonMean.Location = New System.Drawing.Point(12, 12)
-        Me.ButtonMean.Name = "ButtonMean"
-        Me.ButtonMean.Size = New System.Drawing.Size(152, 63)
-        Me.ButtonMean.TabIndex = 0
-        Me.ButtonMean.Text = "MEAN"
-        Me.ButtonMean.UseVisualStyleBackColor = True
-        '
-        'ButtonVariance
-        '
-        Me.ButtonVariance.Location = New System.Drawing.Point(695, 12)
-        Me.ButtonVariance.Name = "ButtonVariance"
-        Me.ButtonVariance.Size = New System.Drawing.Size(152, 63)
-        Me.ButtonVariance.TabIndex = 1
-        Me.ButtonVariance.Text = "VARIANCE"
-        Me.ButtonVariance.UseVisualStyleBackColor = True
+        Me.ButtonMeanVariance.Location = New System.Drawing.Point(349, 12)
+        Me.ButtonMeanVariance.Name = "ButtonMeanVariance"
+        Me.ButtonMeanVariance.Size = New System.Drawing.Size(152, 63)
+        Me.ButtonMeanVariance.TabIndex = 0
+        Me.ButtonMeanVariance.Text = "MEAN AND VARIANCE"
+        Me.ButtonMeanVariance.UseVisualStyleBackColor = True
         '
         'ButtonClose
         '
-        Me.ButtonClose.Location = New System.Drawing.Point(695, 408)
+        Me.ButtonClose.Location = New System.Drawing.Point(863, 526)
         Me.ButtonClose.Name = "ButtonClose"
         Me.ButtonClose.Size = New System.Drawing.Size(152, 63)
         Me.ButtonClose.TabIndex = 2
         Me.ButtonClose.Text = "CLOSE"
         Me.ButtonClose.UseVisualStyleBackColor = True
         '
-        'RichTextBoxMean
+        'RichTextBoxM_V
         '
-        Me.RichTextBoxMean.Location = New System.Drawing.Point(170, 12)
-        Me.RichTextBoxMean.Name = "RichTextBoxMean"
-        Me.RichTextBoxMean.Size = New System.Drawing.Size(181, 63)
-        Me.RichTextBoxMean.TabIndex = 3
-        Me.RichTextBoxMean.Text = ""
-        '
-        'RichTextBoxVariance
-        '
-        Me.RichTextBoxVariance.Location = New System.Drawing.Point(508, 12)
-        Me.RichTextBoxVariance.Name = "RichTextBoxVariance"
-        Me.RichTextBoxVariance.Size = New System.Drawing.Size(181, 63)
-        Me.RichTextBoxVariance.TabIndex = 4
-        Me.RichTextBoxVariance.Text = ""
+        Me.RichTextBoxM_V.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBoxM_V.Location = New System.Drawing.Point(507, 12)
+        Me.RichTextBoxM_V.Name = "RichTextBoxM_V"
+        Me.RichTextBoxM_V.Size = New System.Drawing.Size(340, 63)
+        Me.RichTextBoxM_V.TabIndex = 3
+        Me.RichTextBoxM_V.Text = ""
         '
         'TextBoxN
         '
-        Me.TextBoxN.Location = New System.Drawing.Point(379, 53)
+        Me.TextBoxN.Location = New System.Drawing.Point(121, 32)
         Me.TextBoxN.Name = "TextBoxN"
         Me.TextBoxN.Size = New System.Drawing.Size(94, 22)
         Me.TextBoxN.TabIndex = 5
@@ -88,58 +71,51 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(400, 33)
+        Me.Label1.Location = New System.Drawing.Point(142, 12)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(57, 17)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "Insert N"
         '
-        'PictureBoxMean
+        'ChartMean
         '
-        Me.PictureBoxMean.Location = New System.Drawing.Point(12, 81)
-        Me.PictureBoxMean.Name = "PictureBoxMean"
-        Me.PictureBoxMean.Size = New System.Drawing.Size(339, 306)
-        Me.PictureBoxMean.TabIndex = 7
-        Me.PictureBoxMean.TabStop = False
-        '
-        'PictureBoxVariance
-        '
-        Me.PictureBoxVariance.Location = New System.Drawing.Point(508, 81)
-        Me.PictureBoxVariance.Name = "PictureBoxVariance"
-        Me.PictureBoxVariance.Size = New System.Drawing.Size(339, 306)
-        Me.PictureBoxVariance.TabIndex = 8
-        Me.PictureBoxVariance.TabStop = False
+        ChartArea1.Name = "ChartArea1"
+        Me.ChartMean.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.ChartMean.Legends.Add(Legend1)
+        Me.ChartMean.Location = New System.Drawing.Point(12, 81)
+        Me.ChartMean.Name = "ChartMean"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.ChartMean.Series.Add(Series1)
+        Me.ChartMean.Size = New System.Drawing.Size(835, 508)
+        Me.ChartMean.TabIndex = 7
+        Me.ChartMean.Text = "Chart1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(875, 487)
-        Me.Controls.Add(Me.PictureBoxVariance)
-        Me.Controls.Add(Me.PictureBoxMean)
+        Me.ClientSize = New System.Drawing.Size(1039, 601)
+        Me.Controls.Add(Me.ChartMean)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBoxN)
-        Me.Controls.Add(Me.RichTextBoxVariance)
-        Me.Controls.Add(Me.RichTextBoxMean)
+        Me.Controls.Add(Me.RichTextBoxM_V)
         Me.Controls.Add(Me.ButtonClose)
-        Me.Controls.Add(Me.ButtonVariance)
-        Me.Controls.Add(Me.ButtonMean)
+        Me.Controls.Add(Me.ButtonMeanVariance)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.PictureBoxMean, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBoxVariance, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ChartMean, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents ButtonMean As Button
-    Friend WithEvents ButtonVariance As Button
+    Friend WithEvents ButtonMeanVariance As Button
     Friend WithEvents ButtonClose As Button
-    Friend WithEvents RichTextBoxMean As RichTextBox
-    Friend WithEvents RichTextBoxVariance As RichTextBox
+    Friend WithEvents RichTextBoxM_V As RichTextBox
     Friend WithEvents TextBoxN As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents PictureBoxMean As PictureBox
-    Friend WithEvents PictureBoxVariance As PictureBox
+    Friend WithEvents ChartMean As DataVisualization.Charting.Chart
 End Class
--- HW6/DistributionMeanVariance/Form1.vb
+++ HW6/DistributionMeanVariance/Form1.vb
@@ -1,7 +1,9 @@
-﻿Public Class Form1
-    Dim die As New List(Of Integer)({1, 2, 3, 4, 5, 6})
-    Dim length_die = die.Count
-    Dim p = (1 / length_die)
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class Form1
+    Dim dice As New List(Of Integer)({1, 2, 3, 4, 5, 6})
+    Dim length_dice = dice.Count
+    Dim p = (1 / length_dice)
     Function CheckLimit()
         If String.IsNullOrEmpty(TextBoxN.Text) Then
             MsgBox("Insert N into Box (es. 1,...,1000)")
@@ -15,49 +17,108 @@
         For Each number In list
             m += number * probability
         Next
-        'MsgBox(m)
+        Console.WriteLine(m)
         Return m
     End Function
     Public Function Variance(list As List(Of Integer)) As Double
-        Dim m = Mean(die, p)
+        Dim m = Mean(dice, p)
         Dim v As New Double
         v = 0
         For Each number In list
             v += Math.Pow((m - number), 2) / 6
         Next
+        Console.WriteLine(Math.Round(v, 2))
         'MsgBox(Math.Round(v, 2))
         Return Math.Round(v, 2)
     End Function
-    'Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
-    '    Dim Generator As System.Random = New System.Random()
-    '    Return Generator.Next(Min, Max)
-    'End Function
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonMean.Click
+    Public Function SampleMean(list As List(Of Integer)) As Double
+        Dim m As New Double
+        m = 0
+        Dim n = list.Count
+        For Each number In list
+            m += number / n
+        Next
+        'MsgBox(m)
+        Return Math.Round(m, 2)
+    End Function
+    Public Function SampleVariance(list As List(Of Integer)) As Double
+        Dim sample_mean = SampleMean(list)
+        Dim diff = New Double
+        Dim pow_diff = New Double
+        Dim sum_pow_diff = New Double
+        Dim sample_variance = New Double
+        For Each number In list
+            diff = number - sample_mean
+            pow_diff = Math.Pow((diff), 2)
+            sum_pow_diff += pow_diff
+        Next
+        sample_variance = sum_pow_diff / list.Count
+        Dim rounded_sample_variance = Math.Round(sample_variance, 2)
+        Return rounded_sample_variance
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonMeanVariance.Click
+        ChartMean.Titles.Clear()
+        ChartMean.Series.Clear()
+
         If CheckLimit() Then
             Try
-                Dim n = CInt(TextBoxN.Text)
-                Mean(die, p)
-                Randomize()
-                Dim randoms(n) As Integer
-                For i As Integer = 0 To n - 1
-                    randoms(i) = CInt(Int((n * Rnd()) + 1))
-                Next
+                'CHART
+                ChartMean.Titles.Add("Mean & Variance of the roll dice ")
+                Dim s As New Series
+                s.Name = "Percentage \n of \n outcome"
+                'Change to a line graph.
+                s.ChartType = SeriesChartType.Column
 
-                For i = 0 To n - 1
-                    Console.WriteLine(randoms(i))
-                Next i
-                'MsgBox(array_flip(5))
+                Dim n = CInt(TextBoxN.Text)
+                Mean(dice, p)
+                Variance(dice)
+                Randomize()
+
+                Dim randoms As Integer
+                Dim dictionary = New Dictionary(Of Integer, Integer)
+                For Each number In dice
+                    dictionary.Add(number, 0)
+                Next
+                For i As Integer = 0 To n - 1
+                    'randoms(i) = CInt(Int((n * Rnd()) + 1))
+                    randoms = CInt(Int((length_dice * Rnd()) + 1))
+                    Console.WriteLine(randoms)
+                    dictionary(randoms) += 1
+                Next
+                Console.WriteLine("##############")
+                ' Get list of keys.
+                Dim keys As List(Of Integer) = dictionary.Keys.ToList
+                ' Sort the keys.
+                keys.Sort()
+                Dim list_values_keys = New List(Of Integer)
+                For Each key As Integer In keys
+                    Console.WriteLine(key.ToString & " " & dictionary(key))
+                    list_values_keys.Add(dictionary(key))
+                    s.Points.AddXY(key.ToString, dictionary(key))
+                Next
+                Console.WriteLine("##############")
+                'Mean After Random
+                Dim sample_mean = SampleMean(list_values_keys)
+                RichTextBoxM_V.AppendText("Mean = " & sample_mean & Environment.NewLine)
+                Console.WriteLine(sample_mean)
+                Dim vari = SampleVariance(list_values_keys)
+                RichTextBoxM_V.AppendText("Variance = " & vari)
+                Console.WriteLine(vari)
+                'Variance After Random
+
+                ChartMean.Series.Add(s)
             Catch ex As Exception
                 MsgBox("Insert an Integer and not anymore")
             End Try
         End If
     End Sub
 
-    Private Sub ButtonVariance_Click(sender As Object, e As EventArgs) Handles ButtonVariance.Click
+    Private Sub ButtonVariance_Click(sender As Object, e As EventArgs)
         If CheckLimit() Then
             Try
                 Dim n = CInt(TextBoxN.Text)
-                Variance(die)
+                Variance(dice)
             Catch ex As Exception
                 MsgBox("Insert an Integer and not anymore")
             End Try
@@ -65,5 +126,9 @@
     End Sub
     Private Sub ButtonClose_Click(sender As Object, e As EventArgs) Handles ButtonClose.Click
         Me.Close()
+    End Sub
+
+    Private Sub RichTextBoxMean_TextChanged(sender As Object, e As EventArgs) Handles RichTextBoxM_V.TextChanged
+
     End Sub
 End Class
