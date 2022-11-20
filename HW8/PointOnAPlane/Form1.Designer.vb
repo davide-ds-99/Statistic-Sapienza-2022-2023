47c55520beb03f1128c7cedf4aa915aa5237147a
Davide DS <davidedisilvestro99@gmail.com>
2022-11-20 14:32:27 +0100

    HW8 Point On a Plane

---
+++ HW8/PointOnAPlane/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea3 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend3 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series3 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Chart = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ButtonClose = New System.Windows.Forms.Button()
+        Me.ChartX = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.ChartY = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.Chart, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ChartX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ChartY, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart
+        '
+        Me.Chart.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart.Legends.Add(Legend1)
+        Me.Chart.Location = New System.Drawing.Point(43, 96)
+        Me.Chart.Name = "Chart"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.Chart.Series.Add(Series1)
+        Me.Chart.Size = New System.Drawing.Size(606, 525)
+        Me.Chart.TabIndex = 1
+        Me.Chart.Text = "Chart1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(43, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 78)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "DRAW CARTESIAN PLANE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ButtonClose
+        '
+        Me.ButtonClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonClose.Location = New System.Drawing.Point(1048, 578)
+        Me.ButtonClose.Name = "ButtonClose"
+        Me.ButtonClose.Size = New System.Drawing.Size(89, 43)
+        Me.ButtonClose.TabIndex = 3
+        Me.ButtonClose.Text = "CLOSE"
+        Me.ButtonClose.UseVisualStyleBackColor = True
+        '
+        'ChartX
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.ChartX.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.ChartX.Legends.Add(Legend2)
+        Me.ChartX.Location = New System.Drawing.Point(674, 42)
+        Me.ChartX.Name = "ChartX"
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.ChartX.Series.Add(Series2)
+        Me.ChartX.Size = New System.Drawing.Size(368, 260)
+        Me.ChartX.TabIndex = 4
+        Me.ChartX.Text = "ChartX"
+        '
+        'ChartY
+        '
+        ChartArea3.Name = "ChartArea1"
+        Me.ChartY.ChartAreas.Add(ChartArea3)
+        Legend3.Name = "Legend1"
+        Me.ChartY.Legends.Add(Legend3)
+        Me.ChartY.Location = New System.Drawing.Point(674, 339)
+        Me.ChartY.Name = "ChartY"
+        Series3.ChartArea = "ChartArea1"
+        Series3.Legend = "Legend1"
+        Series3.Name = "Series1"
+        Me.ChartY.Series.Add(Series3)
+        Me.ChartY.Size = New System.Drawing.Size(368, 282)
+        Me.ChartY.TabIndex = 5
+        Me.ChartY.Text = "Chart2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(671, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(192, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "EMPIRICAL DISTRIBUTION X"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(671, 319)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(192, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "EMPIRICAL DISTRIBUTION Y"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1149, 644)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ChartY)
+        Me.Controls.Add(Me.ChartX)
+        Me.Controls.Add(Me.ButtonClose)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Chart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ChartX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ChartY, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Chart As DataVisualization.Charting.Chart
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ButtonClose As Button
+    Friend WithEvents ChartX As DataVisualization.Charting.Chart
+    Friend WithEvents ChartY As DataVisualization.Charting.Chart
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ HW8/PointOnAPlane/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class Form1
+    Dim ArrayX As New List(Of Double)
+    Dim ArrayY As New List(Of Double)
+    Dim n = 50
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Chart.Titles.Clear()
+        Chart.Series.Clear()
+        ArrayX.Clear()
+        ArrayY.Clear()
+
+        'CHART
+        Chart.Titles.Add("Points in a Cartesian Plane")
+        Dim s As New Series
+        'Dim lambda = CInt(TextBoxLambda.Text)
+        s.Name = " n° points = " & n
+        'Change to a line graph.
+        s.ChartType = SeriesChartType.Point
+        s.MarkerStyle = MarkerStyle.Circle
+        s.MarkerSize = 5
+        s.MarkerColor = Color.Red
+        Dim rand As Random = New Random()
+        For k As Integer = 0 To n
+            Dim u1 As Double = rand.NextDouble()
+            Console.WriteLine("U1 = " & u1)
+            Dim u2 As Double = rand.NextDouble()
+            Console.WriteLine("U2 = " & u2)
+            Dim r As Double = Math.Sqrt(-2 * Math.Log10(u1))
+            Console.WriteLine("R = " & r)
+            Dim tetha As Double = 2 * Math.PI * u2
+            Console.WriteLine("Tetha = " & tetha)
+            Dim X As Double = r * Math.Cos(tetha)
+            Dim Y As Double = r * Math.Sin(tetha)
+            Console.WriteLine("X = " & X & " Y = " & Y)
+            ArrayX.Add(X)
+            ArrayY.Add(Y)
+            s.Points.AddXY(X, Y)
+        Next
+        Chart.Series.Add(s)
+        drawEmpiricalDistribution(ArrayX, ChartX)
+        drawEmpiricalDistribution(ArrayY, ChartY)
+    End Sub
+
+    Private Sub drawEmpiricalDistribution(unorderedarray As List(Of Double), chart As Chart)
+        Dim ordered_array As List(Of Double) = unorderedarray
+        ordered_array.Sort()
+        chart.Titles.Clear()
+        chart.Series.Clear()
+        chart.Titles.Add("Empirical Distribution")
+        Dim s As New Series
+        s.ChartType = SeriesChartType.Column
+        's.MarkerStyle = MarkerStyle.Circle
+        s.MarkerSize = 5
+        s.MarkerColor = Color.Green
+        For item_index As Integer = 0 To n
+            Console.WriteLine(item_index, ordered_array(item_index))
+            s.Points.AddXY(item_index, ordered_array(item_index))
+        Next
+        chart.Series.Add(s)
+    End Sub
+
+    Private Sub ButtonClose_Click(sender As Object, e As EventArgs) Handles ButtonClose.Click
+        Close()
+    End Sub
+End Class
